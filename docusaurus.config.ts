ab4b75e54003281caf2c51edf15fb745b1e2e1ca
gonzalomsastre <gonzalo.sastre@creativecoefficient.net>
2024-09-11 03:46:03 -0300

    Deploy attempt

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   favicon: 'img/leo.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://gonzalomartinezs.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/IntroClaseGit/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
